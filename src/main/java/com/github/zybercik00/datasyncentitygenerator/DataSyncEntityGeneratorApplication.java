d7d68ebc13c6d127e191e7a580de2374f9f59974
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-09-20 17:11:36 +0200

    #14 Created a new Spring Application module that will be used to generate entities from the database.

---
+++ src/main/java/com/github/zybercik00/datasyncentitygenerator/DataSyncEntityGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.github.zybercik00.datasyncentitygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSyncEntityGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSyncEntityGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/zybercik00/datasyncentitygenerator/DataSyncEntityGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zybercik00.datasyncentitygenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSyncEntityGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
