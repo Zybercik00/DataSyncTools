95fd3be3c994bb1e3c11fccb4473d811af93753c
OChebanenko <acebanenco@gmail.com>
2023-09-23 11:13:26 +0200

    #14 install sql table, load metadata, action plan

--- src/main/java/com/github/zybercik00/datasyncentitygenerator/DataSyncEntityGeneratorApplication.java
+++ src/main/java/com/github/zybercik00/datasyncentitygenerator/DataSyncEntityGeneratorApplication.java
@@ -1,13 +1,50 @@
 package com.github.zybercik00.datasyncentitygenerator;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@SpringBootApplication
 public class DataSyncEntityGeneratorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DataSyncEntityGeneratorApplication.class, args);
+    public static void main(String[] args) throws SQLException {
+        List<String> tables = new ArrayList<>();
+        // TODO [8] Connection from springboot
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:testdb", "admin", "password")) {
+            //
+            try (Statement statement = connection.createStatement()) {
+                // TODO [9] Install using liquibase
+                statement.execute("""
+                        create table EXTRACTION
+                        (
+                            EXTRACTION_ID    BIGINT not null primary key,
+                            PREPARED_ON        TIMESTAMP,
+                            RECEIVED_IN_BERN   TIMESTAMP,
+                            SAMPLE_TEST_RESULT NUMERIC,
+                            WEIGHT_AFTER       NUMERIC,
+                            WEIGHT_BEFORE      NUMERIC,
+                            MATERIAL           BIGINT
+                        )
+                        """);
+            }
+            DatabaseMetaData metaData = connection.getMetaData();
+            try (ResultSet tableResultSet = metaData.getTables(null, "PUBLIC", null, null)) {
+                while (tableResultSet.next()) {
+                    String tableName = tableResultSet.getString("TABLE_NAME");
+                    tables.add(tableName);
+                }
+            }
+            // TODO [1] fetch columns (names and types)
+            // TODO [5] fetch primary keys
+            // TODO [4] fetch unique indexes
+            // TODO [3] fetch foreign keys
+            // TODO [6] fetch sequences
+        }
+
+        for (String table : tables) {
+            // TODO [7] Use string templates
+            String entityName = table; // TODO [2] Capitalize, FOO_BAR replace with FooBar
+            System.out.printf("public class %s {}%n", entityName);
+        }
     }
 
 }
