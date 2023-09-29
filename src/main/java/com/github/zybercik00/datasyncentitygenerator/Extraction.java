de5d6e5faa930e7f7ae54c09c20a9760eb172822
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-09-30 00:26:12 +0200

    #14 Class created from File Writer

---
+++ src/main/java/com/github/zybercik00/datasyncentitygenerator/Extraction.java
@@ -0,0 +1,14 @@
+package com.github.zybercik00.datasyncentitygenerator;
+
+import java.math.BigDecimal;
+import java.security.Timestamp;
+
+public class Extraction {
+private long extractionId; 
+private Timestamp preparedOn;
+private Timestamp receivedInBern; 
+private BigDecimal sampleTestResult;
+private BigDecimal weightAfter; 
+private BigDecimal weightBefore; 
+private long material; 
+}
